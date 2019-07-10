f07a2602f3513bd14c98d4f1e6beafc73a569339
Michael Aguiar <michael.aguiarr@icloud.com>
2019-07-09 21:19:32 -0400

    Aula 4

--- src/main/java/com/michael/resources/UserResource.java
+++ src/main/java/com/michael/resources/UserResource.java
@@ -2,6 +2,7 @@ package com.michael.resources;
 
 import com.michael.domain.User;
 
+import com.michael.dto.UserDTO;
 import com.michael.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,10 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -22,7 +21,9 @@ public class UserResource {
     private UserService userService;
 
     @RequestMapping(method=RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
-        return ResponseEntity.ok().body(userService.findAll());
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<User> list = userService.findAll();
+        List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
